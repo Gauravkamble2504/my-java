10dd9236d865602997f53be1c4eaa34faf0b4c22
Gauravkamble2504 <112939218+Gauravkamble2504@users.noreply.github.com>
2022-12-27 20:48:20 -0800

    Add files via upload

---
+++ Project/library/library/src/test/java/com/Managment/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Managment.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
